ff4708f474a43476a6d827d7939fbbd9a21e64a4
Thibo Kuijpers <btwthibo@gmail.com>
2024-12-12 17:48:00 +0100

    first commit

--- js/main.js
+++ js/main.js
@@ -12,3 +12,23 @@ function openMenu() {
     }
 
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+    const reveals = document.querySelectorAll(".reveal");
+
+    const observer = new IntersectionObserver(
+        (entries, observer) => {
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                    entry.target.classList.add("active");
+                    observer.unobserve(entry.target); // Stops observing after animation
+                }
+            });
+        },
+        { threshold: 0.1 } // Trigger when 10% of the element is visible
+    );
+
+    reveals.forEach(reveal => {
+        observer.observe(reveal);
+    });
+});
